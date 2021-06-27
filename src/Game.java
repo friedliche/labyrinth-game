3b310b3362bbf55bb983079bc3d7cb181c4b65f4
friedliche <ga63nux@mytum.de>
2021-06-27 10:16:42 +0200

    add game jFrame

---
+++ src/Game.java
@@ -0,0 +1,64 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Game {
+
+    private static ExecutorService threadPool;
+
+    //create container for cards
+    private static CardLayout cl;
+    private static JPanel cardPanel;
+
+
+    private static JFrame createJFrame(){
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1200, 760);
+
+        return frame;
+    }
+
+    public static void main(String[] args) {
+
+        threadPool = Executors.newFixedThreadPool(6);
+
+        cl = new CardLayout();
+        cardPanel = new JPanel(cl);
+
+        TitleScreen titleScreenPanel = new TitleScreen();
+        StartScreen startScreenPanel = new StartScreen();
+        SettingsScreen settingsScreenPanel = new SettingsScreen();
+        HelpScreen helpScreenPanel = new HelpScreen();
+        PlayScreen playScreenPanel = new PlayScreen();
+        WinScreen winScreenPanel = new WinScreen();
+        cardPanel.add(playScreenPanel, "playScreen");
+        cardPanel.add(titleScreenPanel, "titleScreen");
+        cardPanel.add(settingsScreenPanel, "settingsScreen");
+        cardPanel.add(helpScreenPanel, "helpScreen");
+        cardPanel.add(startScreenPanel, "startScreen");
+        cardPanel.add(winScreenPanel, "winScreen");
+
+
+        JFrame frame = createJFrame();
+        frame.add(cardPanel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static CardLayout getCl(){
+        return cl;
+    }
+
+    public static JPanel getCardPanel(){
+        return cardPanel;
+    }
+
+    public static ExecutorService getThreadPool(){
+        return threadPool;
+    }
+}
