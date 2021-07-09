08dfbbfa54181034ad5a7a297d7d493795e735c3
friedliche <ga63nux@mytum.de>
2021-07-09 23:41:19 +0200

    make startscreen skippable

--- src/StartScreen.java
+++ src/StartScreen.java
@@ -1,34 +1,40 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 
+import static helper.Constants.*;
+
 public class StartScreen extends AbstractScreen {
+
+    private static final int CONDITION = JComponent.WHEN_IN_FOCUSED_WINDOW;
 
     private BufferedImage bloodImage;
     private Timer timer;
     private int yDelta = 3 * 2;
     private int yPos = 100;
 
-    public StartScreen(){
+    public StartScreen() {
         super(new GridBagLayout());
 
         try {
-            bloodImage =  ImageIO.read(StartScreen.class.getResource("/resources/pics/startLabel.png"));
+            bloodImage = ImageIO.read(StartScreen.class.getResource("/resources/pics/startLabel.png"));
         } catch (Exception e) {
             System.out.println("Error occured: " + e);
         }
 
+        this.getInputMap(CONDITION).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), ESC_KEY);
+        this.getActionMap().put(ESC_KEY, key_esc);
+
         this.addComponentListener(new ComponentListener() {
             @Override
-            public void componentResized(ComponentEvent e) { }
+            public void componentResized(ComponentEvent e) {
+            }
 
             @Override
-            public void componentMoved(ComponentEvent e) { }
+            public void componentMoved(ComponentEvent e) {
+            }
 
             @Override
             public void componentShown(ComponentEvent e) {
@@ -37,12 +43,20 @@ public class StartScreen extends AbstractScreen {
 
             @Override
             public void componentHidden(ComponentEvent e) {
-                if (!(timer==null)){
+                if (!(timer == null)) {
                     timer.stop();
                 }
             }
         });
     }
+
+    private Action key_esc = new AbstractAction(ESC_KEY) {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            yPos = 100;
+            Game.getCl().show(Game.getCardPanel(), "playScreen");
+        }
+    };
 
     public Dimension getPreferredSize() {
         return new Dimension(300, 100);
@@ -57,7 +71,6 @@ public class StartScreen extends AbstractScreen {
                 if (yPos + bloodImage.getHeight() > getHeight() - 200) {
                     yPos = 100;
                     Game.getCl().show(Game.getCardPanel(), "playScreen");
-                    setFocusable(true);
                 }
                 repaint();
             }
